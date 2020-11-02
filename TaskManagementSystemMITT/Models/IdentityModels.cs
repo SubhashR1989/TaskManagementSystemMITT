6a5b3701ed28f300cbb9044723c20cea4fa82b41
yuxin <yuxinli915@outlook.com>
2020-11-02 10:13:48 -0600

    Created ProjectHelper Class

--- TaskManagementSystemMITT/Models/IdentityModels.cs
+++ TaskManagementSystemMITT/Models/IdentityModels.cs
@@ -20,6 +20,7 @@ namespace TaskManagementSystemMITT.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<ProjectHelper> Projects { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
---
+++ TaskManagementSystemMITT/Models/ProjectHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManagementSystemMITT.Models
+{
+    public class ProjectHelper
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ProjectManagerId { get; set; }
+        public ApplicationUser ProjectManager { get; set; }
+        //public ICollection<Task> Tasks { get; set; }
+        //public ProjectHelper()
+        //{
+        //    Tasks = new HashSet<Task>();
+        //}
+    }
+}
