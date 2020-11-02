87ee44450792dea78a913365055525efe79faa20
Kevin Luong <luongk88@gmail.com>
2020-11-01 18:56:33 -0600

    Created UserRoleHelper Class

---
+++ TaskManagementSystemMITT/Models/UserRoleHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManagementSystemMITT.Models
+{
+    public static class UserRoleHelper
+    {
+        static ApplicationDbContext db = new ApplicationDbContext();
+        static UserManager<ApplicationUser> userManager = new UserManager<ApplicationUser>
+            (new UserStore<ApplicationUser>(db));
+        static RoleManager<IdentityRole> roleManager = new RoleManager<IdentityRole>
+            (new RoleStore<IdentityRole>(db));
+
+        public static void AddRole(string roleName)
+        {
+            if(!roleManager.RoleExists(roleName))
+            {
+                roleManager.Create(new IdentityRole { Name = roleName });
+            }
+
+        }
+    }
+}
