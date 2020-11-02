f07a17c721351b36047a6084d7d9894864cc6e96
Kevin Luong <luongk88@gmail.com>
2020-11-02 15:01:46 -0600

    Created CreateUser,DeleteUser,AllUsersInRole,CheckIfUserInRole Methods

--- TaskManagementSystemMITT/Models/UserRoleHelper.cs
+++ TaskManagementSystemMITT/Models/UserRoleHelper.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 
 namespace TaskManagementSystemMITT.Models
 {
@@ -17,21 +18,68 @@ namespace TaskManagementSystemMITT.Models
 
         public static void AddRole(string roleName)
         {
-            if(!roleManager.RoleExists(roleName))
-            {
+            if (!roleManager.RoleExists(roleName))
                 roleManager.Create(new IdentityRole { Name = roleName });
-            }
-
         }
 
         public static void DeleteRole(string roleName)
         {
-            if(roleManager.RoleExists(roleName))
+            if (roleManager.RoleExists(roleName))
             {
                 var role = roleManager.FindByName(roleName);
                 roleManager.Delete(role);
-                
             }
+        }
+
+        public static void UpdateRole(string userId, string roleName)
+        {
+            var user = userManager.FindById(userId);
+            var currentUserRole = Roles.GetRolesForUser(user.UserName).ToString();
+
+            if (!roleManager.RoleExists(roleName))
+                roleManager.Create(new IdentityRole { Name = roleName });
+
+            userManager.RemoveFromRole(userId, currentUserRole);
+            userManager.AddToRole(userId, roleName);
+        }
+
+        public static void AddUserToRole(string userId, string roleName)
+        {
+            if (!userManager.IsInRole(userId, roleName))
+                userManager.AddToRole(userId, roleName);
+        }
+
+        public static bool CheckIfUserInRole(string userId, string roleName)
+        {
+            var result = userManager.IsInRole(userId, roleName);
+
+            return result;
+        }
+
+        public static ICollection<string> AllUsersInRole(string roleName)
+        {
+            var result = Roles.GetUsersInRole(roleName);
+
+            return result;
+        }
+
+        public static ApplicationUser CreateUser(string Email, string roleName)
+        {
+            var user = new ApplicationUser() { UserName = Email, Email = Email };
+            userManager.AddToRole(user.Id, roleName);
+
+            return user;
+        }
+
+        public static void DeleteUser(string userId)
+        {
+            var user = userManager.FindById(userId);
+            var currentUserRole = Roles.GetRolesForUser(user.UserName).ToString();
+
+            userManager.RemoveFromRole(userId, currentUserRole);
+            userManager.Delete(user);
+
         }
     }
 }
+
