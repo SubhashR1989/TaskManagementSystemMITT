f8c79c276551188fe2d0eba8b8c999b7150dd042
DamiBiu <64222443+DamiBiu@users.noreply.github.com>
2020-11-03 11:16:50 -0600

    commit

---
+++ TaskManagementSystemMITT/Models/ProjectTask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace TaskManagementSystemMITT.Models
+{
+    public class ProjectTask
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public TimeSpan Duration { get; set; }
+
+        public virtual Project Project { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+        public virtual ProjectTaskStatus Status { get; set; }
+    }
+}
