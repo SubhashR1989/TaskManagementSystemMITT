e85ee3bd02fe9535b3d4af77b356e62b70b80a82
yuxin <yuxinli915@outlook.com>
2020-11-03 15:07:43 -0600

    Uncomment content in projectHelper class

--- TaskManagementSystemMITT/Models/ProjectHelper.cs
+++ TaskManagementSystemMITT/Models/ProjectHelper.cs
@@ -9,41 +9,41 @@ namespace TaskManagementSystemMITT.Models
     {
         static ApplicationDbContext db = new ApplicationDbContext();
 
-        //public static bool CreateProject(string name, string projectManagerId)
-        //{
-        //    if (db.Projects.Any(p => p.Name == name))
-        //    {
-        //        return false;
-        //    }
-        //    db.Projects.Add(new Project() { Name = name, ProjectManagerId = projectManagerId });
-        //    return true;
-        //}
+        public static bool CreateProject(string name, string projectManagerId)
+        {
+            if (db.Projects.Any(p => p.Name == name))
+            {
+                return false;
+            }
+            db.Projects.Add(new Project() { Name = name, ProjectManagerId = projectManagerId });
+            return true;
+        }
 
-        //public static bool DeleteProject(int id)
-        //{
-        //    if (db.Projects.Any(p => p.Id == id))
-        //    {
-        //        var proj = db.Projects.Find(id);
-        //        db.Projects.Remove(proj);
-        //        return true;
-        //    }
-        //    return false;
-        //}
+        public static bool DeleteProject(int id)
+        {
+            if (db.Projects.Any(p => p.Id == id))
+            {
+                var proj = db.Projects.Find(id);
+                db.Projects.Remove(proj);
+                return true;
+            }
+            return false;
+        }
 
-        //public static bool EditProject(int id, string name)
-        //{
-        //    if (db.Projects.Any(p => p.Id == id) && !db.Projects.Any(p=>p.Name == name))
-        //    {
-        //        var proj = db.Projects.Find(id);
-        //        proj.Name = name;
-        //        return true;
-        //    }
-        //    return false;
-        //}
+        public static bool EditProject(int id, string name)
+        {
+            if (db.Projects.Any(p => p.Id == id) && !db.Projects.Any(p => p.Name == name))
+            {
+                var proj = db.Projects.Find(id);
+                proj.Name = name;
+                return true;
+            }
+            return false;
+        }
 
-        //public static List<ProjectTask> AllTasksByProject(int id)
-        //{
-        //    return db.Projects.Find(id).ProjectTasks.toList();
-        //}
+        public static List<ProjectTask> AllTasksByProject(int id)
+        {
+            return db.Projects.Find(id).ProjectTasks.toList();
+        }
     }
 }
