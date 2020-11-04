7c11e2cf1faf5c013178c4c24fd5121d8ec5585d
DamiBiu <64222443+DamiBiu@users.noreply.github.com>
2020-11-04 11:06:59 -0600

    Created TaskHelper Project

---
+++ TaskManagementSystemMITT/Models/TaskHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManagementSystemMITT.Models
+{
+    public class TaskHelper
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ProjectManagerId { get; set; }
+        public ApplicationUser ProjectManager { get; set; }
+}
